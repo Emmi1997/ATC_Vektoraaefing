d8f4105d151103f8a0ea8192de7bcda73ee8afbb
Emmi1997 <emile20@ru.is>
2022-02-22 15:07:18 +0000

    ye

---
+++ Assets/AirPlane_Collission.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirPlane_Collission_yellow : MonoBehaviour
+{
+    public Sprite normal;
+    public Sprite conflict;
+    public Sprite danger;
+    public SpriteRenderer render;
+
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.transform.CompareTag("AirPlane"))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = conflict;
+        }
+
+        if (collider.transform.CompareTag("DangerZone"))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = danger;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = normal;
+        
+    }
+
+}
---
+++ Assets/AirPlane_Collission_yellow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AirPlane_Collission : MonoBehaviour
+{
+    public Sprite normal;
+    public Sprite conflict;
+    public Sprite danger;
+    public SpriteRenderer render;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.transform.CompareTag("AirPlane"))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = conflict;
+        }
+
+        if (collider.transform.CompareTag("DangerZone"))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = danger;
+        }
+
+       /* if (collider.transform.CompareTag("Crash"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+       */
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = normal;
+        
+    }
+
+}
